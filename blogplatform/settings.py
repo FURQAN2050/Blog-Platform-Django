95eec8c29f4b195a94b09ab5472b55d1b1d4a63e
furqan2050 <mfurqan@folio3.com>
2021-11-18 17:00:14 +0500

    authentication.

--- blogplatform/settings.py
+++ blogplatform/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
+    'ROTATE_REFRESH_TOKENS': True,
+}
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.staticfiles',
     'blog',
-    'rest_framework'
+    'rest_framework',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +75,14 @@ TEMPLATES = [
         },
     },
 ]
+# REST_FRAMEWORK = {
+#     'DEFAULT_FILTER_BACKENDS': [
+#         'django_filters.rest_framework.DjangoFilterBackend'
+#     ],
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ],
+# }
 
 WSGI_APPLICATION = 'blogplatform.wsgi.application'
 
--- blogplatform/urls.py
+++ blogplatform/urls.py
@@ -20,5 +20,6 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
+    path('authentication/', include('authentication.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
