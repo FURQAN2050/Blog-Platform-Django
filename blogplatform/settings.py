1b0827aa8e2f91d80bf5f84cf4ed8c7a16b33d40
furqan2050 <mfurqan@folio3.com>
2021-11-22 21:42:10 +0500

    STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"

--- blogplatform/settings.py
+++ blogplatform/settings.py
@@ -146,5 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 
 django_heroku.settings(locals())
