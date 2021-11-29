7a3b17a74f26569506fd7b334c186b6b18412b68
furqan2050 <mfurqan@folio3.com>
2021-11-29 22:25:59 +0500

    bug fix.

--- blogplatform/urls.py
+++ blogplatform/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path("", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("blog/", include("blog.urls")),
     path("authentication/", include("authentication.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
